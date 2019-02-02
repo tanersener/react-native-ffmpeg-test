214dcf9abdc8f96214805dda22452c188aac4d0f
Taner Sener <tanersener@gmail.com>
2019-02-02 18:02:30 +0300

    add clean script to delete all binary files

---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf default/node_modules
+rm -rf default/ios/build
+rm -rf default/ios/Pods
+rm -rf default/android/app/build
+
+rm -rf ios-frameworks/node_modules
+rm -rf ios-frameworks/ios/build
+rm -rf ios-frameworks/ios/Pods
