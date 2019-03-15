8defa8b0106ba258b9ffb86c232c11988b82239d
Taner Sener <tanersener@gmail.com>
2019-03-15 13:44:37 +0300

    remove tests due to react-navigation errors

--- typescript/jest/setup.js
+++ typescript/jest/setup.js
@@ -29,14 +29,3 @@ jest.mock('react-navigation', () => {
         createBottomTabNavigator: jest.fn(),
     }
 });
-
-const {defaults: tsjPreset} = require('ts-jest/presets');
-
-module.exports = {
-    ...tsjPreset,
-    preset: 'react-native',
-    transform: {
-        ...tsjPreset.transform,
-        '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
-    }
-};
