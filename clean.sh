e7aac00c12c18b97ff433a0d610536d2156caa94
Taner Sener <tanersener@gmail.com>
2019-05-17 22:28:03 +0300

    update versions

--- clean.sh
+++ clean.sh
@@ -8,3 +8,8 @@ rm -rf default/android/app/build
 rm -rf ios-frameworks/node_modules
 rm -rf ios-frameworks/ios/build
 rm -rf ios-frameworks/ios/Pods
+
+rm -rf typescript/node_modules
+rm -rf typescript/ios/build
+rm -rf typescript/ios/Pods
+rm -rf typescript/android/app/build
