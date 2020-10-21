6ea6c7b5a7989861d99672b4826146d4f8a9d0ea
Taner Sener <tanersener@gmail.com>
2020-10-22 00:56:14 +0100

    use only one test application

--- clean.sh
+++ clean.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-rm -rf default/node_modules
-rm -rf default/ios/build
-rm -rf default/ios/Pods
-rm -rf default/android/app/build
-
-rm -rf ios-frameworks/node_modules
-rm -rf ios-frameworks/ios/build
-rm -rf ios-frameworks/ios/Pods
-
-rm -rf typescript/node_modules
-rm -rf typescript/ios/build
-rm -rf typescript/ios/Pods
-rm -rf typescript/android/app/build
+rm -rf node_modules
+rm -rf ios/build
+rm -rf ios/Pods
+rm -rf android/app/build
